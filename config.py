1c8012316d8dfe423bb938fb44a028ffb3818e05
Churkabesina <148402010+Churkabesina@users.noreply.github.com>
2023-12-04 09:44:42 +0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+mods_path = r''  # Путь до модов, которые нужно перевести | Example: r'D:\ModsToTranslate'
+
+translated_mods_path = r'D:\ModsTranslated'  # Путь, куда сохранить переведенные моды
+
+o_auth = ''  # Oauth токен от аккаунта яндекс клауд | Example: 'y0_AgBBAALmCwFAWTuwQAFFGAaASDAWaMZ_mQw-WjfS73CicbOz0VASDw'
+
+yandex_cloud_folder_id = ''  # id Папки в яндекс клауд с доступом к translate api | Example: 'a1gdqqxi95s7ljdf7dd3f'
